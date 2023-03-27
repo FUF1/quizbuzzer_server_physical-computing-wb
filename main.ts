4b554ed19102c1407ddcc7fc382648d9a8a518b7
FUF1 <69962736+FUF1@users.noreply.github.com>
2023-03-27 19:06:11 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,38 @@ radio.onReceivedNumber(function (receivedNumber) {
     entries.push(radio.receivedPacket(RadioPacketProperty.SerialNumber))
     music.playTone(262, music.beat(BeatFraction.Eighth))
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+    winningPlayerList = []
+    for (let Index = 0; Index <= entries.length - 1; Index++) {
+        let currentEntry = 0
+        if (entries[currentEntry] == -1907160588) {
+            winningPlayerList.push("A")
+            if (entries[currentEntry] == 0) {
+                winningPlayerList.push("B")
+                if (entries[currentEntry] == 0) {
+                    winningPlayerList.push("C")
+                    if (entries[currentEntry] == 0) {
+                        winningPlayerList.push("D")
+                        if (entries[currentEntry] == 0) {
+                            winningPlayerList.push("E")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (let Index = 0; Index <= entries.length - 1; Index++) {
+        sendText = "" + sendText + "; " + entries[Index]
+    }
+    serial.writeLine(sendText)
+    basic.pause(100)
+    basic.clearScreen()
+    while (!(input.buttonIsPressed(Button.A))) {
+        basic.showString("" + (winningPlayerList[0]))
+    }
+    basic.clearScreen()
+    music.playMelody("G B A G C5 B A B ", 600)
     entries = []
     sendText = ""
     basic.showIcon(IconNames.House)
@@ -15,16 +46,7 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     music.playMelody("C5 A B G A F G E ", 600)
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Happy)
-    for (let Index = 0; Index <= entries.length; Index++) {
-        sendText = "sendText" + "; " + entries[Index]
-    }
-    serial.writeLine("" + (sendText))
-    basic.pause(100)
-    basic.clearScreen()
-    music.playMelody("G B A G C5 B A B ", 600)
-})
+let winningPlayerList: string[] = []
 let sendText = ""
 let entries: number[] = []
 radio.setGroup(4)
