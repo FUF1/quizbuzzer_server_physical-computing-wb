33a8a1a29f0a31e346faa3b93e1ed4387915c80e
FUF1 <69962736+FUF1@users.noreply.github.com>
2023-03-27 19:47:56 +0200

    working just fine without processing

--- main.ts
+++ main.ts
@@ -6,45 +6,47 @@ radio.onReceivedNumber(function (receivedNumber) {
     music.playTone(262, music.beat(BeatFraction.Eighth))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Happy)
-    winningPlayerList = []
-    for (let Index = 0; Index <= entries.length - 1; Index++) {
-        let currentEntry = 0
-        if (entries[currentEntry] == -1907160588) {
-            winningPlayerList.push("A")
-            if (entries[currentEntry] == 0) {
+    if (entries.length > 0) {
+        basic.showIcon(IconNames.Happy)
+        winningPlayerList = []
+        for (let Index = 0; Index <= entries.length - 1; Index++) {
+            let currentEntry = 0
+            if (entries[currentEntry] == -1907160588) {
+                winningPlayerList.push("A")
+            } else if (entries[currentEntry] == -1372141027) {
                 winningPlayerList.push("B")
-                if (entries[currentEntry] == 0) {
-                    winningPlayerList.push("C")
-                    if (entries[currentEntry] == 0) {
-                        winningPlayerList.push("D")
-                        if (entries[currentEntry] == 0) {
-                            winningPlayerList.push("E")
-                        }
-                    }
-                }
+            } else if (entries[currentEntry] == 943271416) {
+                winningPlayerList.push("C")
+            } else if (entries[currentEntry] == 1472197505) {
+                winningPlayerList.push("D")
+            } else if (entries[currentEntry] == -1850258811) {
+                winningPlayerList.push("E")
+            } else {
+                winningPlayerList.push("")
             }
         }
+        for (let Index = 0; Index <= winningPlayerList.length - 1; Index++) {
+            sendText = "" + sendText + "; " + winningPlayerList[Index]
+        }
+        serial.writeLine(sendText)
+        entries = []
+        basic.pause(100)
+        basic.clearScreen()
+        while (!(input.buttonIsPressed(Button.A))) {
+            basic.showString("" + (winningPlayerList[0]))
+        }
+        basic.clearScreen()
+        music.playMelody("G B A G C5 B A B ", 600)
+        sendText = ""
+        basic.showIcon(IconNames.House)
+        basic.pause(100)
+        basic.showIcon(IconNames.Yes)
+        basic.pause(100)
+        basic.clearScreen()
+        music.playMelody("C5 A B G A F G E ", 600)
+    } else {
+        music.playTone(147, music.beat(BeatFraction.Half))
     }
-    for (let Index = 0; Index <= entries.length - 1; Index++) {
-        sendText = "" + sendText + "; " + entries[Index]
-    }
-    serial.writeLine(sendText)
-    basic.pause(100)
-    basic.clearScreen()
-    while (!(input.buttonIsPressed(Button.A))) {
-        basic.showString("" + (winningPlayerList[0]))
-    }
-    basic.clearScreen()
-    music.playMelody("G B A G C5 B A B ", 600)
-    entries = []
-    sendText = ""
-    basic.showIcon(IconNames.House)
-    basic.pause(100)
-    basic.showIcon(IconNames.Yes)
-    basic.pause(100)
-    basic.clearScreen()
-    music.playMelody("C5 A B G A F G E ", 600)
 })
 let winningPlayerList: string[] = []
 let sendText = ""
